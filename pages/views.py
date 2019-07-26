916cd6b1c51754e28f808054a3a80323ab3ef90a
Wu, Lei <znss1989@gmail.com>
2019-07-26 16:43:37 +0800

    have dynamic realtors in about page

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 # Create your views here.
@@ -14,4 +15,11 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
