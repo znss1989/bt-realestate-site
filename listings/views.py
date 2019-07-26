d45e5babb560f7d826f96a10496ac94e262cdbe7
Wu, Lei <znss1989@gmail.com>
2019-07-26 15:59:14 +0800

    sort listings and filter to publish or not

--- listings/views.py
+++ listings/views.py
@@ -6,8 +6,8 @@ from .models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
-    paginator = Paginator(listings, 3)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
