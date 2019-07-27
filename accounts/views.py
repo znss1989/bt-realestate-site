d2c0191ef4aa45ad501f2c7eadd59a3924e15a54
Wu, Lei <znss1989@gmail.com>
2019-07-27 15:20:58 +0800

    validate register info

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # validation
+        if (password != password2):
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'User already exists')
+            return redirect('register')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email being used')
+            return redirect('register')
     return render(request, 'accounts/register.html')
 
 
