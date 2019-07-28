5668aa8ae4bb8330b2d6ea8ea504282907800800
Wu, Lei <znss1989@gmail.com>
2019-07-28 10:32:09 +0800

    detect existing inquiry

--- contacts/views.py
+++ contacts/views.py
@@ -15,8 +15,14 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # check if user make same inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'Have inquired this already')
+                return redirect('/listings/' + listing_id)
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
         messages.success(request, 'Request sent, a realtor will get to you soon.')
         return redirect('/listings/' + listing_id)
-    return
